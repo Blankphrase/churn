b42d08efa5fe8bec2f83c294fc9d29ac9a0a8da2
edwardcooper <maxminicherrycc@gmail.com>
2017-12-07 15:51:58 -0500

    split the data.

--- 3_Baseline_model/1_baseline_models_training.R
+++ 3_Baseline_model/1_baseline_models_training.R
@@ -1,2 +1,2 @@
 # let us try soem baseline models like random forest, xgbTree, xgbLinear, xgbDart. 
-source('churn/2_Data_Clean/data_clean_1.R', echo=TRUE)
+source("https://raw.githubusercontent.com/edwardcooper/churn/master/2_Data_Clean/data_clean_1.R")
